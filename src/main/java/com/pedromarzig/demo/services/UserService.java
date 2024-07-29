8527215a353fe1c939fd8e082fc2eb8ef74803de
pedro marzig <pedromarzigcastelani@gmail.com>
2024-07-29 15:56:47 -0300

    Exception handling - delete

--- src/main/java/com/pedromarzig/demo/services/UserService.java
+++ src/main/java/com/pedromarzig/demo/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.pedromarzig.demo.entitties.User;
 import com.pedromarzig.demo.repositories.UserRepository;
+import com.pedromarzig.demo.services.exceptions.DatabaseException;
 import com.pedromarzig.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+			
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
